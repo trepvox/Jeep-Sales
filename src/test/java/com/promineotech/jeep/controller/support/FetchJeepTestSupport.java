7cfbbcc02b1d34391e86d340a2196b947f269efe
trepvox <smylesmoriarty@gmail.com>
2021-09-03 12:00:35 -0400

    all this weeks video updates

--- src/test/java/com/promineotech/jeep/controller/support/FetchJeepTestSupport.java
+++ src/test/java/com/promineotech/jeep/controller/support/FetchJeepTestSupport.java
@@ -1,8 +1,12 @@
 package com.promineotech.jeep.controller.support;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import java.math.BigDecimal;
+import java.util.Collections;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Map;
+import org.springframework.http.HttpStatus;
 import com.promineotech.jeep.entity.Jeep;
 import com.promineotech.jeep.entity.JeepModel;
 
@@ -29,10 +33,44 @@ public class FetchJeepTestSupport extends BaseTest {
     // @formatter:on
     
     
-    
+    Collections.sort(list);
     return list;
   }
 
+  /**
+   * 
+   * @param error
+   * @param status
+   */
+  protected void assertErrorMessage(Map<String, Object> error, 
+      HttpStatus status) {
+    //@formatter:off
+     assertThat(error)
+       .containsKey("message")
+       .containsEntry("status code", status.value())
+       .containsEntry("uri", "/jeeps")
+       .containsKey("timestamp")
+       .containsEntry("reason", status.getReasonPhrase());
+     //@formatter:on
+  }
+//  I
+  
+  /**
+   * 
+   * @param error
+   * @param status
+   */
+  protected void assertErrorMessageValid(Map<String, Object> error, 
+      HttpStatus status) {
+    //@formatter:off
+     assertThat(error)
+       .containsKey("message")
+       .containsEntry("status code", status.value())
+       .containsEntry("uri", "/jeeps")
+       .containsKey("timestamp")
+       .containsEntry("reason", status.getReasonPhrase());
+     //@formatter:on
+  }
 //  INSERT INTO models (model_id, trim_level, num_doors, wheel_size, base_price) VALUES('WRANGLER', 'Sport', 2, 17, 28475.00);
 //  INSERT INTO models (model_id, trim_level, num_doors, wheel_size, base_price) VALUES('WRANGLER', 'Sport', 4, 17, 31975.00);
 }
